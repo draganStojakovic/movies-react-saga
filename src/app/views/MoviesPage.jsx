c3976b97914c6d1044cdeb9e649d80d7b9ae4e54
qouillyan <stojakovicdragan98@gmail.com>
2023-01-22 11:41:21 +0100

    zaboravio provider

--- src/app/views/MoviesPage.jsx
+++ src/app/views/MoviesPage.jsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeSelectMovies } from "../store/movies/selector";
+import { getAllMoviesAction } from "../store/movies/slice";
+
 export const MoviesPage = () => {
+  const movies = useSelector(makeSelectMovies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllMoviesAction());
+  }, []);
+
+  console.log(movies);
+
   return <h1>MoviesPage</h1>;
 };
